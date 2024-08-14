9acf6f4f52c35866861251b98037c803ff7e95e6
Shipu Ahamed <shipu@10minuteschool.com>
2024-08-14 16:57:42 +0600

    add postgres and sqlite db connection

--- relation_db.go
+++ relation_db.go
@@ -2,6 +2,8 @@ package artifact
 
 import (
 	"gorm.io/driver/mysql"
+	"gorm.io/driver/postgres"
+	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
 
@@ -13,7 +15,19 @@ type Database struct {
 
 func NewDatabase() *Database {
 	dsn := Config.GetString(Config.RelationDBConfig+".Username") + ":" + Config.GetString(Config.RelationDBConfig+".Password") + "@tcp(" + Config.GetString(Config.RelationDBConfig+".Host") + ":" + Config.GetString(Config.RelationDBConfig+".Port") + ")/" + Config.GetString(Config.RelationDBConfig+".Database") + "?charset=utf8mb4&parseTime=True&loc=Local"
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	connection := Config.GetString(Config.RelationDBConfig + ".Connection")
+	err := error(nil)
+	var db *gorm.DB
+	if connection == "mysql" {
+		dsn = Config.GetString(Config.RelationDBConfig+".Username") + ":" + Config.GetString(Config.RelationDBConfig+".Password") + "@tcp(" + Config.GetString(Config.RelationDBConfig+".Host") + ":" + Config.GetString(Config.RelationDBConfig+".Port") + ")/" + Config.GetString(Config.RelationDBConfig+".Database") + "?charset=utf8mb4&parseTime=True&loc=Local"
+		db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	} else if connection == "sqlite" {
+		dsn = Config.GetString(Config.RelationDBConfig + ".Database")
+		db, err = gorm.Open(sqlite.Open(dsn), &gorm.Config{})
+	} else if connection == "postgres" {
+		dsn = "host=" + Config.GetString(Config.RelationDBConfig+".Host") + " user=" + Config.GetString(Config.RelationDBConfig+".Username") + " password=" + Config.GetString(Config.RelationDBConfig+".Password") + " dbname=" + Config.GetString(Config.RelationDBConfig+".Database") + " port=" + Config.GetString(Config.RelationDBConfig+".Port") + " sslmode=disable TimeZone=Asia/Shanghai"
+		db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	}
 
 	if err != nil {
 		panic(err)
