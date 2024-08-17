764a739179222a8edfdd91417bff3f2f54db401e
Md. Toufiqul Islam <toufiq.austcse@gmail.com>
2024-08-17 15:46:23 +0600

    feat: add error handling

--- relation_db.go
+++ relation_db.go
@@ -1,7 +1,7 @@
 package artifact
 
 import (
-	"fmt"
+	"errors"
 	"gorm.io/driver/mysql"
 	"gorm.io/driver/postgres"
 	"gorm.io/driver/sqlite"
@@ -18,7 +18,7 @@ type Database struct {
 	*gorm.DB
 }
 
-func NewDatabase() *Database {
+func NewDatabase() (*Database, error) {
 	dsn := Config.GetString(Config.RelationDBConfig+".Username") + ":" + Config.GetString(Config.RelationDBConfig+".Password") + "@tcp(" + Config.GetString(Config.RelationDBConfig+".Host") + ":" + Config.GetString(Config.RelationDBConfig+".Port") + ")/" + Config.GetString(Config.RelationDBConfig+".Database") + "?charset=utf8mb4&parseTime=True&loc=Local"
 	connection := Config.GetString(Config.RelationDBConfig + ".Connection")
 	config := &gorm.Config{}
@@ -39,22 +39,24 @@ func NewDatabase() *Database {
 
 	err := error(nil)
 	var db *gorm.DB
-	if connection == "mysql" {
+
+	switch connection {
+	case "mysql":
 		dsn = Config.GetString(Config.RelationDBConfig+".Username") + ":" + Config.GetString(Config.RelationDBConfig+".Password") + "@tcp(" + Config.GetString(Config.RelationDBConfig+".Host") + ":" + Config.GetString(Config.RelationDBConfig+".Port") + ")/" + Config.GetString(Config.RelationDBConfig+".Database") + "?charset=utf8mb4&parseTime=True&loc=Local"
 		db, err = gorm.Open(mysql.Open(dsn), config)
-	} else if connection == "sqlite" {
-		dsn = Config.GetString(Config.RelationDBConfig + ".Database")
-		db, err = gorm.Open(sqlite.Open(dsn), config)
-	} else if connection == "postgres" {
+	case "postgres":
 		dsn = "host=" + Config.GetString(Config.RelationDBConfig+".Host") + " user=" + Config.GetString(Config.RelationDBConfig+".Username") + " password=" + Config.GetString(Config.RelationDBConfig+".Password") + " dbname=" + Config.GetString(Config.RelationDBConfig+".Database") + " port=" + Config.GetString(Config.RelationDBConfig+".Port") + " sslmode=disable TimeZone=Asia/Shanghai"
 		db, err = gorm.Open(postgres.Open(dsn), config)
-	} else {
-		fmt.Println("no connection found")
+	case "sqlite":
+		dsn = Config.GetString(Config.RelationDBConfig + ".Database")
+		db, err = gorm.Open(sqlite.Open(dsn), config)
+	default:
+		err = errors.New("db connection not supported")
+
 	}
 
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
-
-	return &Database{db}
+	return &Database{db}, nil
 }
