ce4ff44f1f1ece35d041e4ca2c30909e951caec4
Md. Toufiqul Islam <toufiq.austcse@gmail.com>
2024-08-20 18:01:36 +0600

    refact: pagination

--- pagination.go
+++ pagination.go
@@ -10,24 +10,41 @@ import (
 var Pagination paginator.Paginator
 
 type PaginationMeta struct {
-	CurrentPage int `json:"current_page"`
-	PerPage     int `json:"per_page"`
-	LastPage    int `json:"last_page"`
-	Total       int `json:"total"`
+	CurrentPage int   `json:"current_page"`
+	PerPage     int   `json:"per_page"`
+	LastPage    int   `json:"last_page"`
+	Total       int64 `json:"total"`
 }
 
 type Paginator struct {
 	Meta PaginationMeta `json:"meta"`
+	db   *gorm.DB
 	*paginator.Paginator
+	model  interface{}
+	filter map[string]interface{}
 }
 
-func (paginator *Paginator) PaginateScope() func(db *gorm.DB) *gorm.DB {
+func (paginator *Paginator) PaginateScope(page int, limit int) func(db *gorm.DB) *gorm.DB {
+	if page == 0 {
+		page = 1
+	}
+	if limit == 0 {
+		limit = 10
+	}
+	var totalRows int64
+
+	paginator.Meta.CurrentPage = page
+	paginator.Meta.PerPage = limit
+
+	DB.Model(paginator.model).Where(paginator.filter).Count(&totalRows)
+	paginator.Meta.Total = totalRows
+	paginator.Meta.LastPage = int(math.Ceil(float64(totalRows) / float64(limit)))
+
 	return func(db *gorm.DB) *gorm.DB {
 		offset := (paginator.Meta.CurrentPage - 1) * paginator.Meta.PerPage
 		return db.Offset(offset).Limit(paginator.Meta.PerPage)
 	}
 }
-
 func (paginator *Paginator) CursorPaginate(query *gorm.DB, v interface{}) (*gorm.DB, paginator.Cursor, error) {
 	return paginator.Paginate(query, v)
 }
@@ -55,20 +72,13 @@ func (paginator *Paginator) updateMeta(v interface{}, request map[string]interfa
 	paginator.Meta.CurrentPage = int(page)
 	paginator.Meta.PerPage = int(limit)
 
-	countFilterMap := make(map[string]interface{})
-	for k, v := range request {
-		if k != "page" && k != "limit" {
-			countFilterMap[k] = v
-		}
-	}
-	
 	var totalRows int64
-	DB.Model(v).Where(countFilterMap).Count(&totalRows)
-	paginator.Meta.Total = int(totalRows)
+	DB.Model(v).Count(&totalRows)
+	paginator.Meta.Total = totalRows
 	paginator.Meta.LastPage = int(math.Ceil(float64(totalRows) / float64(limit)))
 }
 
-func NewPaginator(v interface{}, request map[string]interface{}) *Paginator {
+func NewPaginator(v interface{}, queryFilter map[string]interface{}) *Paginator {
 	opts := []paginator.Option{
 		&paginator.Config{
 			Order: paginator.ASC,
@@ -77,8 +87,6 @@ func NewPaginator(v interface{}, request map[string]interface{}) *Paginator {
 
 	p := paginator.New(opts...)
 
-	newInstance := &Paginator{Meta: PaginationMeta{}, Paginator: p}
-	newInstance.updateMeta(v, request)
-
+	newInstance := &Paginator{Meta: PaginationMeta{}, Paginator: p, model: v, filter: queryFilter}
 	return newInstance
 }
